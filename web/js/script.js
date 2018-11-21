2dbf2aefe5d1d80103508321154facc64e98bae7
gjiayi <jiayi_gan@mymail.sutd.edu.sg>
2018-11-21 12:39:42 +0800

    Prototype with search counter using local storage

--- web/js/script.js
+++ web/js/script.js
@@ -14,28 +14,35 @@
     s.parentNode.insertBefore(gcse, s);
 })();
 
-function countsearch()
-        {
-            var counter = parseInt(sessionStorage.getItem('counter'));
-            counter +=1;
-            $(".counter-area").text(counter);
-            sessionStorage.setItem('counter', counter);
-        };
+function countsearch(parent) {
+    var prev = parent.prev();
+    var input = prev.find("input");
+    console.log(input.val().length)
+    if (input.val().length !== 0) {
+        var counter = parseInt(localStorage.getItem('counter'));
+        counter += 1;
+        $(".counter-area").text(counter);
+        localStorage.setItem('counter', counter);
+    }
+}
 
 $(document).ready(function () {
-    if (sessionStorage.getItem('counter') == null){
-        sessionStorage.setItem('counter', 0);
+    if (localStorage.getItem('counter') === null) {
+        localStorage.setItem('counter', 0);
         $(".counter-area").text("Search away!");
     } else {
-        var counter = parseInt(sessionStorage.getItem('counter'));
+        var counter = parseInt(localStorage.getItem('counter'));
         $(".counter-area").text(counter);
     }
+
     $(window).click(function (e) {
-        console.log(e.target);
-        if (e.target.matches(".gsc-search-button")) {
-            $(".gsc-search-button").on('click', countsearch());
-        }
-        if (e.target.matches("#menuButton") || e.target.matches(".bar1") || e.target.matches(".bar2") || e.target.matches(".bar3")) {
+        if ($(e.target).is("svg")) {
+            var parent = $(e.target).parent().parent();
+            countsearch(parent);
+        } else if (e.target.matches(".gsc-search-button")) {
+            var parent = $(e.target).parent();
+            countsearch(parent);
+        } else if (e.target.matches("#menuButton") || e.target.matches(".bar1") || e.target.matches(".bar2") || e.target.matches(".bar3")) {
             $("#menuButton").toggleClass("change");
             $("#myDropdown").toggleClass("show");
         } else {
